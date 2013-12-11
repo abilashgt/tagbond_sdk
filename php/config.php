6d3bc5acbfb58692b94041b52f020e0ddc872f01
abilash <abilash@hifx.net>
2013-12-11 11:57:19 +0530

    tagbond php sdk update

---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+ini_set('display_errors', 1);
+date_default_timezone_set('Asia/Manila');
+
+$clientId = 'your-client-id';
+$clientSecret = 'your-client-secret';
+$redirectUri = 'your-redirect-uri';
+//$scopes = array('scope1','scope2');
+?>
--- php/demo.php
+++ php/demo.php
@@ -1,57 +1,56 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);
-ini_set('display_errors', 1);
-date_default_timezone_set('Asia/Manila');
-
 session_start();
 
+//creating tagbond sdk object
 include 'lib/Tagbond.php';
-
-$clientId = 'your-client-id';
-$clientSecret = 'your-client-secret';
-$redirectUri = 'your-redirect-uri';
-
 $tagbond = new Tagbond;
-$tagbond->setClient($clientId,$clientSecret);
-$tagbond->setRedirect($redirectUri);
-//$tagbond->setScopes(array('scope1','scope2'));
 
-//checking redirect uri
-if(Tagbond::currentUrl() != $redirectUri){
-	echo "Incorrect redirect uri. Please put this file on the right path where your redirect uri points";
-	exit;
-}
-
-if(!$tagbond->isLoggedIn()){
-	$url = $tagbond->getLoginUrl();
-	if($tagbond->getResponseType() == 'code'){
-		header("Location: ".$url);
-		exit;
-	}
-	else {
-		echo $url;
-	}
+//fetching the configurations
+if(file_exists(dirname(__FILE__).'/config-testing.php')){
+	$config=dirname(__FILE__).'/config-testing.php';
 }
 else{
-	$userDetails = $tagbond->getData('user/profile');
-	//$userDetails = $tagbond->getUser();
-
-	if($userDetails){
-		echo '<pre>';
-		print_r($userDetails);
-		echo '</pre>';
-		exit;
-	}
-	else{
-		exit('error:no_token');
-	}
+	$config=dirname(__FILE__).'/config.php';
 }
+require $config;
+
+//setting the configurations
+$tagbond->setClient($clientId,$clientSecret);
+$tagbond->setRedirect($redirectUri);
+$tagbond->setScopes($scopes);
 ?>
 
 <html>
 <head><title>Tagbond SDK</title></head>
 <body>
+	<?php
+	//checking redirect uri
+	$currentUrl = Tagbond::currentUrl();
+	if(strpos($currentUrl, $redirectUri) === false){
+		echo "Incorrect redirect uri. Please put this file on the right path where your redirect uri points";
+		exit;
+	}
+
+	if(!$tagbond->isLoggedIn()){
+		$url = $tagbond->getLoginUrl();
+	}
+	else{
+		$userDetails = $tagbond->getData('user/profile');
+		//$userDetails = $tagbond->getUser();
+
+		if($userDetails){
+			echo '<pre>';
+			print_r($userDetails);
+			echo '</pre>';
+			exit;
+		}
+		else{
+			exit('error:no_token');
+		}
+	}
+	?>
 	<div>
+		For reqular user's to login and use Tagbond<br />
 		<input type="submit" value="Login with Tagbond" onClick="parent.location='<?php echo $url ?>'">
 	</div>
 </body>
